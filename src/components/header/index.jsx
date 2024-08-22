a5c898bc41803347ffacae0f2522904afaaed189
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 04:41:53 -0700

    update header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import logo from '../../assets/img/logo.svg';
+import { Nav } from './nav';
+import { Social } from './social';
+import { NavMobile } from './navMobile/navMobile';
 
 export const Header = () => {
+
+    const [bg,setBg] = useState(false);
+
+    useEffect(() =>{
+        window.addEventListener('scroll', () =>{
+            return window.scrollY > 50 ? setBg(true) : setBg(false);
+        }
+        )
+    })
   return (
-    <div>
-      Header
-    </div>
+    <header className={`${bg ? "bg-tertiary h-20" : "h-24"} flex items-center fixed top-0 w-full
+     text-white z-10 transition-all duration-300`}>
+        <div className="container mx-auto h-full flex
+        items-center justify-between">
+            <a href="#">
+                <img src={logo} alt="Logo" />
+            </a>
+            <div className="hidden lg:block">
+                <Nav/>
+            </div>
+            <div className="hidden lg:block">
+                <Social/>
+            </div>
+            <div className="hidden lg:block">
+                <NavMobile/>
+            </div>
+        </div>
+    </header>
   )
 }
 
--- src/components/header/social.jsx
+++ src/components/header/social.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export const Nav = () => {
+export const Social = () => {
   return (
     <div>
-      Nav
+      Social
     </div>
   )
 }
+
 
