71ef25c181b1d55bb79e0a47c75c70f6effb9928
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 06:56:33 -0700

    update hero component

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return (
     <section id="home" className="lg:h-[85vh] flex items-center bg-primary lg:bg-cover 
     lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden">
-        <div className=" container mx-auto">Hero</div>
+        {/* <div className=" container mx-auto">Hero</div> */}
 
         <div className="container mx-auto h-full">
             <div className=" flex items-center h-full
@@ -20,6 +20,13 @@ export const Hero = () => {
                     md:text-5xl md:leading-tight lg:text-7xl
                     lg:leading-[1.2] font-body md:tracking-[-2px]">
                         I Build & Design <br/> Eb Interfaces</h1>
+                    <p className="pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, beatae.
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, beatae.
+                    </p>
+                    <button className="btn btn-md bg-accent
+                    hover:bg-accent-hover md:btn-lg
+                    transition-all">Work with me</button>
                 </div>
 
                 <div className="hidden lg:flex flex-1 justify-end
