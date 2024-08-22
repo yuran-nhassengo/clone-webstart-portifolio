86f00e101f2962047fa406500bf8714e7c7faecb
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 15:35:13 -0700

    add Project component

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 import {projectsData} from "../../data"
 import {projectsNav} from "../../data"
+import { Project } from './project';
 
 export const Projects = () => {
 
@@ -10,17 +11,44 @@ export const Projects = () => {
     const [projects,setProjects] =useState([]);
     const [active,setActive] = useState(0);
 
+    useEffect(()=>{
+        if(item.name == "all"){
+            setProjects(projectsData);
+        }else{
+            const newProject = projectsData.filter((project)=> {return project.category.toLowerCase()
+        === item.name;
+            });
+            setProjects(newProject);
+        }
+    },[item]);
+
+    const handleClick = (e,index) =>{
+        setItem({name: e.target.textContent.toLowerCase()});
+        setActive(index);
+    }
+
   return (
     <div>
-      <nav>
-        <ul>
+      <nav className="mb-12 max-w-xl mx-auto">
+        <ul className="flex flex-col md:flex-row
+        justify-evenly items-center text-white">
 
             {projectsNav.map((item,index) => {
-                return <li>{item.name}</li>
+                return <li onClick={(e) => {
+                    handleClick(e,index);
+                }}
+                className={`${active === index ? "active" : ""} cursor-pointer
+                capitalize m-4`}
+                 key={index}>{item.name}</li>
             })}
             
         </ul>
       </nav>
+       <section>
+        {projects.map((item)=>{
+            return <Project item={item} key={item.id}/>
+        })}
+       </section>
     </div>
   )
 }
---
+++ src/components/projects/project.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Project = ({item}) => {
+  return (
+    <div key={item.id} className="flex flex-col items-center text-center">
+       
+    </div>
+  )
+}
+
+
