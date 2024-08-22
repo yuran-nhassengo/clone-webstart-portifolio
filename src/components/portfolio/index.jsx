4b895a41a388654ce4c53a3f8cc651669e0ecc20
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 15:01:00 -0700

    update portfolio layout

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
+
+import { Projects } from '../projects' 
+
 export const Portifolio = () => {
   return (
-    <div>
-       Portifolio
-    </div>
+    <section id='portfolio' className="section bg-primary
+    min-h-[1400px]">
+            <div className="container mx-auto">
+                <div className="flex flex-col items-center">
+                    <h2 className="section-title before:content-portfolio 
+                    relative  before:absolute before:opacity-40
+                    before:-top-[2rem] before:-left-3/4
+                    before:hidden before:lg:block">My latest work</h2>
+                </div>
+            </div>
+    </section>
   )
 }
 
