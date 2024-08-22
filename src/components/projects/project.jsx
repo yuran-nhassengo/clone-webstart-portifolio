692263f2bbb2e51bc835723e751cfee4dccf8561
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 15:53:03 -0700

    update project component

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -4,9 +4,11 @@ export const Project = ({item}) => {
   return (
     <div key={item.id} className="flex flex-col items-center text-center">
         <div className="mb-8">
-            <img src={item.image} alt="projectImage" />
+            <img className="rounded-2xl" src={item.image} alt="projectImage" />
         </div>
-       
+        <p className="capitalize text-accent text-sm
+        mb-3">{item.category}</p>
+        <h3 className="text-2xl font-semibold mb-3">{item.name}</h3>
     </div>
   )
 }
