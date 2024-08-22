51c56bae6d9b2530ecd7022d64f1817387680fe1
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 16:58:50 -0700

    update testimonials

--- src/components/testimonials/testimonial.jsx
+++ src/components/testimonials/testimonial.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import { TestimonialSlider } from './testimonialSlider'
 
 
 
 
-export const Testimonial = () => {
+export const Testimonials = () => {
   return (
-    <div>
-      Testimonial
-    </div>
+    <section id='testimonials' className="section bg-secondary">
+        <div className="container mx-auto">
+            <div className="flex flex-col items-center text-center">
+                <h2 className="section-title before:content-testimonials relative 
+                before:absolute before:opacity-40
+                before:-top-[2rem] before:-left-64 before:hidden
+                before:lg:block">What other people say</h2>
+                <p className="subtitle">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+            <TestimonialSlider/>
+        </div>
+    </section>
   )
 }
 
