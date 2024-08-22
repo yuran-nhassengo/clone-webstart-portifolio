5133b5be0bbede543bb54d2c0d750a47318adfb8
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 05:27:29 -0700

    add social icons

--- src/components/header/social.jsx
+++ src/components/header/social.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import {social} from '../../data'
+
 export const Social = () => {
   return (
-    <div>
-      Social
-    </div>
+    <ul className="flex space-x-6">
+      {social.map((item,index) => {
+          return <li className="flex justify-center
+          items-center text-accent " key={index}>
+            <a className="text-base" href={item.href}>
+              {item.icon}
+            </a>
+          </li>
+      })}
+    </ul>
   )
 }
 
