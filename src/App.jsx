50d14f28d02cda4f74f629ca74bea1808c754eaa
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 05:19:42 -0700

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Header } from './components/header'
+import { Hero } from './components/hero/hero'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
    <Header/>
+   <Hero/>
    <div style={{height: '2000px'}}></div>
     </>
   )
