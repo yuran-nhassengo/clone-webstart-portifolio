06fa7373116333f5a89ce9b7bb6f7eb917fb300f
yurannhassengo <yurannhassengo@gmail.com>
2024-08-22 14:32:06 -0700

    update skill component

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -7,11 +7,13 @@ export const Skills = () => {
   return (
     <section className="bg-tertiary py-12">
             <div className="container mx-auto">
-                <div>
+                <div className="grid grid-cols-8 md:grid-flow-row">
                     {skills.map((item,index) =>{
-                        return <div>
-                            <img src={item.image} alt="skill" />
-                        </div>
+                        return (
+                            <div className="flex items-center justify-center" key={index}>
+                                <img src={item.image} alt="skill" />
+                            </div>
+                            );
                     })}
                 </div>
             </div>
